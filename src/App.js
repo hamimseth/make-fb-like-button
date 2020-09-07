283aa86dd7d4f5038f5b7abfaf574a8204ee2d72
hamimseth <hamim72400@gmail.com>
2020-09-07 21:15:05 +0600

    +handleMuipleData&nesterDataAccess

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+  const [users, setUsers] = useState([]);
+  const [singleUsers, setSingleUsers] = useState({});
+  const [randomUsers, setRandomUsers] = useState({});
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data =>setUsers(data))
+     
+    //singleUsers
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.json())
+    .then(data => setSingleUsers(data))
+
+    //randomUsers
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => setRandomUsers(data.results[0]))
+  },[])
+  const handleLike = () =>{
+    const color = likeColor ?'':'primary';
+    setLikeColor(color);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AccessAlarmIcon></AccessAlarmIcon>
+      <ThumbUpAltIcon onClick={handleLike} color={likeColor}></ThumbUpAltIcon>
+      <h1>Name:{singleUsers.name}</h1>
+      <h2>Random Name:{randomUsers.name && randomUsers.name.first}</h2>
+      {
+        users.map(user =><li>{user.name}</li>)
+      }
     </div>
   );
-}
+};
 
 export default App;
